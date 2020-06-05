8b7cc80d2352e4064bd86363a7a47f4c872c729d
Ching(Nora) Lau <chingwlau@gmail.com>
2020-06-04 22:41:36 -0400

    update

--- src/component/ZipSearch.js
+++ src/component/ZipSearch.js
@@ -6,11 +6,11 @@ export default class ZipSearch extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      zipCodes: {this.props.zip},
+      zipCodes: props.zip,
       cityInfo: null};
   }
 
-  componentDidMount(eachZip) {
+  componentDidMount=(eachZip)=> {
     let url = `http://ctp-zip-api.herokuapp.com/zip/${eachZip}`;
 
     axios
@@ -30,32 +30,32 @@ export default class ZipSearch extends Component {
 
 
   render() {
+    let zipList;
     if (this.state.zipCodes.length === 0) {
       zipList = <></>;
     } else {
       zipList = (
         <ol>
-          {this.state.zipCodes.map((eachZip) => (componentDidMount(eachZip)))}
+          {this.state.zipCodes.map((eachZip) => (eachZip.componentDidMount()))}
         </ol>
       );
     }
-    let zipList;
     const result=()=>
       this.state.zipCodes.map((each) =>{
         return(
           <ZipCard 
-            location= {each.LocationText},
+            location= {each.LocationText}
             population= {each.EstimatedPopulation}
             totalwage= {each.TotalWages}
             />
         );
       })
-      };
-
     return (
-      <div className="Result">
+      <>
+        <div className="Result">
         <p>{result}</p>
-      </div>
+        </div>
+      </>
     );
-  }
+}
 }
